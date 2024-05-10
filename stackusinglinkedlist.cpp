f6b2f9a408c738aba9162aa03cd8395df7975c1a
rnktlhr <rinakitluhur10@gmail.com>
2024-05-10 14:24:49 +0700

    menambahkan case 2 pada switch

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -71,10 +71,18 @@ int main(){
 
         switch(choice){
             case 1:
-            cout << "Enter the value to push: ";
-            cin >> value;
-            stack.push(value);
-            break;
+                cout << "Enter the value to push: ";
+                cin >> value;
+                stack.push(value);
+                break;
+            case 2:
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                else{
+                    cout << "Stack is empty. Cannot pop." << endl;
+                }
+                break;
         }
     }
 }
