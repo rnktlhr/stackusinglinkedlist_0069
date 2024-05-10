0556bcbe55df918a8569b29d5a16e9dfd082a3a0
rnktlhr <rinakitluhur10@gmail.com>
2024-05-10 14:12:55 +0700

    menambahkan else pada void peek

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -40,6 +40,14 @@ class Stack{
             if(top == NULL){
                 cout << "List is empty." << endl;
             }
+            else{
+                Node* current = top;
+                while(current != NULL){
+                    cout << current->data << " " << endl;
+                    current = current->next;
+                }
+                cout << endl;
+            }
         }
 };
 
