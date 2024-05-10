cabe2091a625f7706b8da1b0ac134f54d8f97616
rnktlhr <rinakitluhur10@gmail.com>
2024-05-10 14:02:17 +0700

    melengkapi prosedur int push

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -20,6 +20,11 @@ class Stack{
         }
 
         int push(int value){
-            
+            Node* newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push value: " << value << endl;
+            return value;
         }
 };
