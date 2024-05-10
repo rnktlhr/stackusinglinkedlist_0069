c3a99d2d6237766f9e3f93e5a82ff56f8e1f8813
rnktlhr <rinakitluhur10@gmail.com>
2024-05-10 14:29:45 +0700

    menambahkan defaul pada switch

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -94,6 +94,9 @@ int main(){
             case 4: 
                 cout << "Exiting program." << endl;
                 break;
+            default:
+                cout << "Invailid choice. Try again." << endl;
+                break;
         }
     }
 }
